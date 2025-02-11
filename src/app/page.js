7e79639ddbe0f023937bb7580a51bc04323af5f9
Abhishek Sharma <abhishek.webdev001@gmail.com>
2025-02-11 14:15:00 +0530

    auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import React from "react";
+import { toast } from "react-toastify";
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const Page = () => {
+  const router = useRouter();
+  const { logout, isAuthenticated, user, loading } = useAuth();
+
+  const handleLogout = async () => {
+    const response = await logout();
+    if (response.success) {
+      toast.success("Logout successful!");
+      router.push("/login");
+    } else {
+      toast.error(response.message);
+    }
+  };
+
+  if (loading) {
+    return (
+      <main className="h-screen w-full flex flex-col items-center justify-center">
+        <p>Loading...</p>
+      </main>
+    );
+  }
+
+  return (
+    <main className="h-screen w-full flex flex-col items-center justify-center gap-8">
+      <h1>
+        {isAuthenticated ? `Welcome, ${user?.email}` : "You must log in first."}
+      </h1>
+
+      {isAuthenticated ? (
+        <button
+          className="rounded-md border border-zinc-700 hover:bg-zinc-800 text-[.8rem] px-4 py-2 bg-zinc-900"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      ) : (
+        <Link
+          className="rounded-md border border-zinc-700 hover:bg-zinc-800 text-[.8rem] px-4 py-2 bg-zinc-900"
+          href={"/login"}
+        >
+          Login
+        </Link>
+      )}
+    </main>
+  );
 };
 
-export default page;
+export default Page;
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,114 @@
+// useAuth hook
+
+"use client";
+
+import { useEffect, useState } from "react";
+import { apiURL } from "../utils/constants";
+
+export const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch(`${apiURL}/api/auth/me`, {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setIsAuthenticated(true);
+          setUser(data.user);
+        }
+      } catch (error) {
+        console.error("Auth check failed:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  const signup = async (name, email, password) => {
+    try {
+      const response = await fetch(`${apiURL}/api/auth/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      if (response.status === 409)
+        return { success: response.success, message: response.message };
+
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      return { success: false, message: "Signup failed." };
+    }
+  };
+
+  const login = async (email, password) => {
+    try {
+      const response = await fetch(`${apiURL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setIsAuthenticated(true);
+        setUser(data.user); // Set user info
+        document.cookie = `token=${data.token}; path=/;`;
+      }
+
+      return data;
+    } catch (error) {
+      return { success: false, message: "Login failed" };
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const response = await fetch(`${apiURL}/api/auth/logout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setIsAuthenticated(false);
+        setUser(null);
+        document.cookie =
+          "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+      }
+
+      return data;
+    } catch (error) {
+      return { success: false, message: "Logout failed" };
+    }
+  };
+
+  return {
+    signup,
+    login,
+    logout,
+    isAuthenticated,
+    user,
+    loading,
+  };
+};
